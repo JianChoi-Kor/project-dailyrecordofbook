808c62312a950ec567458e3c35a43d53b0945063
JianChoi-Kor <wldks79@gmail.com>
2021-02-26 20:25:13 +0900

    20210226

--- src/main/java/com/jian/project1/board/BoardController.java
+++ src/main/java/com/jian/project1/board/BoardController.java
@@ -1,10 +1,21 @@
 package com.jian.project1.board;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+		@GetMapping("/write")
+		public void write() {
+			
+		}
+		
+		
+		
+		
+		
+		
 }
